c03380118c8c0d978c10075b4fa234195fbc8e78
kavishka dishan <kavishkadishan19@gmail.com>
2026-09-17 22:20:43 +0530

    Implement student record management using linked list

--- src/student/Student.java
+++ src/student/Student.java
@@ -1,3 +1,56 @@
+package student;
+
 public class Student {
-    
+
+    private int studentId;
+    private String name;
+    private String programme;
+    private double marks;
+
+    public Student(int studentId, String name, String programme, double marks) {
+        this.studentId = studentId;
+        this.name = name;
+        this.programme = programme;
+        this.marks = marks;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProgramme() {
+        return programme;
+    }
+
+    public void setProgramme(String programme) {
+        this.programme = programme;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void setMarks(double marks) {
+        this.marks = marks;
+    }
+
+    public void displayStudent() {
+        System.out.println("------------------------------------------");
+        System.out.println("Student ID : " + studentId);
+        System.out.println("Name       : " + name);
+        System.out.println("Programme  : " + programme);
+        System.out.println("Marks      : " + marks);
+    }
 }
--- src/student/StudentNode.java
+++ src/student/StudentNode.java
@@ -1,4 +1,28 @@
+package student;
 
 public class StudentNode {
-    
+
+    private Student student;
+    private StudentNode next;
+
+    public StudentNode(Student student) {
+        this.student = student;
+        this.next = null;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public StudentNode getNext() {
+        return next;
+    }
+
+    public void setNext(StudentNode next) {
+        this.next = next;
+    }
 }
