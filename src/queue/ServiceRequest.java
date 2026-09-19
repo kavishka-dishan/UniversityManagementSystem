655ae78bd80d0f101d449ba49d8ed03eda3f698b
kavishka dishan <kavishkadishan19@gmail.com>
2026-09-19 19:52:58 +0530

    Integrate final menu and system functionality

--- src/queue/ServiceRequest.java
+++ src/queue/ServiceRequest.java
@@ -1,4 +1,33 @@
+package queue;
 
 public class ServiceRequest {
-    
+
+    private int requestId;
+    private int studentId;
+    private String requestType;
+
+    public ServiceRequest(int requestId, int studentId, String requestType) {
+        this.requestId = requestId;
+        this.studentId = studentId;
+        this.requestType = requestType;
+    }
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getRequestType() {
+        return requestType;
+    }
+
+    public void displayRequest() {
+        System.out.println("------------------------------------------");
+        System.out.println("Request ID   : " + requestId);
+        System.out.println("Student ID   : " + studentId);
+        System.out.println("Request Type : " + requestType);
+    }
 }
